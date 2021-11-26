54e21d3c245a6d71aae9a970910ecbad974449c1
Yah <68326047+YaraProenca@users.noreply.github.com>
2021-11-26 14:41:00 -0300

    :sparkles: Working on API.
    Adding table and working on code.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-const express = require('express')
+// import { openDb } from "./configDB.js";
+import { createTable } from "./controller/movie.js";
+
+import express from 'express'
+const app = express()
+app.use(express.json())
+
+createTable()
+// openDb()
+// const port = 3000
+
+app.get('/', function (req, res) {
+  res.send('Hi there')
+})
+
+app.post('/movie', function (req, res) {
+  console.log(req.body);
+  // insertMovie(req.body)
+  res.json({
+    "statusCode": 200
+  })
+})
+
+app.listen(3000, () => console.log('api working'))
