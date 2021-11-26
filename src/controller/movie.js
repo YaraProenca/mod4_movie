6fe9d8ae1aaf24fafa65406159b7debf44e6d4f6
Yah <68326047+YaraProenca@users.noreply.github.com>
2021-11-26 17:50:08 -0300

    :sparkles: API enhancement.
    Working in progress.

--- src/controller/movie.js
+++ src/controller/movie.js
@@ -6,35 +6,52 @@ export async function createTable() {
   })
 }
 
-export async function insertMovie(movie) {
+export async function selectMovie(req, res) {
+  let id = req.body.id
+  openDb().then(db => {
+    db.get('SELECT * FROM movie WHERE id=?', [id])
+      .then(movie => res.json(movie))
+  })
+}
+
+export async function insertMovie(req, res) {
+  let movie = req.body
   openDb().then(db => {
     db.run('INSERT INTO movie(filme, classificacao, sinopse, genero) VALUES (?,?,?,?)', [movie.filme, movie.classificacao, movie.sinopse, movie.genero])
   })
+
+  res.json({
+    "statusCode": 200
+  })
 }
 
-export async function updateMovie(movie) {
+
+export async function selectMovies(req, res) {
+  openDb().then(db => {
+    db.all('SELECT * FROM movie')
+      .then(movies => res.json(movies))
+  })
+}
+
+export async function updateMovie(req, res) {
+  let movie = req.body
   openDb().then(db => {
     db.run('UPDATE movie SET filme=?, classificacao=?, sinopse=?, genero=? WHERE id=?', [movie.filme, movie.classificacao, movie.sinopse, movie.genero, movie.id])
   })
-}
 
-export async function selectMovies() {
-  return openDb().then(db => {
-    return db.all('SELECT * FROM movie')
-      .then(res => res)
+  res.json({
+    "statusCode": 200
   })
 }
 
-export async function selectMovie(id) {
-  return openDb().then(db => {
-    return db.get('SELECT * FROM movie WHERE id=?', [id])
+export async function deleteMovie(req, res) {
+  let id = req.body.id
+  openDb().then(db => {
+    db.get('DELETE FROM movie WHERE id=?', [id])
       .then(res => res)
   })
-}
 
-export async function deleteMovie(id) {
-  return openDb().then(db => {
-    return db.get('DELETE FROM movie WHERE id=?', [id])
-      .then(res => res)
+  res.json({
+    "statusCode": 200
   })
 }
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { createTable, insertMovie, updateMovie, selectMovies, selectMovie, deleteMovie } from "./controller/movie.js";
+
+const router = Router()
+
+router.get('/', (req, res) => {
+  res.json({
+    "statusCode": 200,
+    " msg": "API Working"
+  })
+})
+
+router.get('/movies', selectMovies)
+router.get('/movie', selectMovie)
+router.post('/movie', insertMovie)
+router.put('/movie', updateMovie)
+router.delete('/movie', deleteMovie)
+
+export default router
