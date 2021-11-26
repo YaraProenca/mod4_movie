75811d6e7786e49c7b1a6096813250fe0626e9a9
Yah <68326047+YaraProenca@users.noreply.github.com>
2021-11-26 15:33:55 -0300

    :art: Working on API code, performace and structure.
    Working in progress.

--- src/controller/movie.js
+++ src/controller/movie.js
@@ -5,3 +5,36 @@ export async function createTable() {
     db.exec('CREATE TABLE IF NOT EXISTS movie (id INTEGER PRIMARY KEY AUTOINCREMENT, filme TEXT, classificacao TEXT, sinopse TEXT, genero TEXT)')
   })
 }
+
+export async function insertMovie(movie) {
+  openDb().then(db => {
+    db.run('INSERT INTO movie(filme, classificacao, sinopse, genero) VALUES (?,?,?,?)', [movie.filme, movie.classificacao, movie.sinopse, movie.genero])
+  })
+}
+
+export async function updateMovie(movie) {
+  openDb().then(db => {
+    db.run('UPDATE movie SET filme=?, classificacao=?, sinopse=?, genero=? WHERE id=?', [movie.filme, movie.classificacao, movie.sinopse, movie.genero, movie.id])
+  })
+}
+
+export async function selectMovies() {
+  return openDb().then(db => {
+    return db.all('SELECT * FROM movie')
+      .then(res => res)
+  })
+}
+
+export async function selectMovie(id) {
+  return openDb().then(db => {
+    return db.get('SELECT * FROM movie WHERE id=?', [id])
+      .then(res => res)
+  })
+}
+
+export async function deleteMovie(id) {
+  return openDb().then(db => {
+    return db.get('DELETE FROM movie WHERE id=?', [id])
+      .then(res => res)
+  })
+}
